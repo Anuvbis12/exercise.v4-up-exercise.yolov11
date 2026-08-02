deee48c4b5fb066f30ebe43517ef0657ed8a9b6d
PutraAlk <putrameygold@gmail.com>
2026-08-02 10:03:26 +0700

    feat: implement batch training script and model training artifacts for YOLOv11-Pose

--- Kodingan/Kodingan/batch_training.py
+++ Kodingan/Kodingan/batch_training.py
@@ -9,14 +9,14 @@ def train_yolo_batch():
         #'yolo11n-pose.pt', 
         #'yolo11s-pose.pt', 
         #'yolo11m-pose.pt', 
-        #'yolo11l-pose.pt', 
-        'yolo11x-pose.pt'
+        'yolo11l-pose.pt', 
+        #'yolo11x-pose.pt'
     ]
     
     # Path ke file data.yaml pada dataset Roboflow
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     data_yaml = os.path.join(base_dir, "exercise.v4-up-exercise.yolov8", "data.yaml")
-    epochs = 50 # Tetap 50 epoch sesuai keinginan
+    epochs = 200 # Tetap 50 epoch sesuai keinginan
     imgsz = 640
     device = 0 # 0
     optimizer = "AdamW"
